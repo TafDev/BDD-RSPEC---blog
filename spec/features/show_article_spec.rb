abe7c7429ebf4cd8375581ef0b01cc0f8592eaf7
TafDev <chombot@hotmail.com>
2017-01-05 20:36:32 +0000

    ensure logged in user can edit their posts

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -26,4 +26,15 @@ RSpec.feature "Show Article" do
 		expect(page).not_to have_link("Edit Article")
 		expect(page).not_to have_link("Delete Article")
 	end
+
+	scenario "show edit and delete buttons for owner" do
+		login_as(@john)
+		visit '/'
+		click_link @article.title
+		expect(page).to have_content(@article.title)
+		expect(page).to have_content(@article.body)
+		expect(current_path).to eq(article_path(@article))
+		expect(page).to have_link("Edit Article")
+		expect(page).to have_link("Delete Article")
+	end
 end
