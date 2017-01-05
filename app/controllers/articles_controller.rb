3c5671f56d50119153d43cb32303c70f41c84416
TafDev <chombot@hotmail.com>
2017-01-05 01:16:18 +0000

    create article successfully

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+	before_action :find_article, only: [:show]
+
+  def index
+  end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save
+		flash[:success] = "Article has been created"
+		redirect_to articles_path
+	end
+
+  def show
+
+  end
+
+	private
+  def find_article
+		@article = Article.find(params[:id])
+  end
+
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
+end
